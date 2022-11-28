5734f2576cbfafd80cb79843632ff68e00af6a86
Greg Cain <codegregcode@gmail.com>
2022-11-28 15:36:33 +0000

    completes 5th (power) and 6th (round) test

--- src/numbers.js
+++ src/numbers.js
@@ -15,11 +15,11 @@ function divide (a, b) {
 }
 
 function power (a, b) {
-  // your code here
+  return a ** b;
 }
 
 function round (a) {
-  // your code here
+  return Math.round(a);
 }
 
 function roundUp (a) {
