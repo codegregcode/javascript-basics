3b501156cfb1f6439958ff8aa5c5f3809edb56b6
Greg Cain <codegregcode@gmail.com>
2022-11-28 15:10:22 +0000

    completes code for first four string tests

--- src/strings.js
+++ src/strings.js
@@ -1,18 +1,18 @@
 function sayHello (string) {
-  // your code here
+  return `Hello, ${string}!`;
 };
 
 function uppercase (string) {
-  // your code here
+  return string.toUpperCase();
 };
 
 function lowercase (string) {
-  // your code here
+  return string.toLowerCase();
 };
 
 function countCharacters (string) {
-  // your code here
-};
+  return string.length;
+  };
 
 function firstCharacter (string) {
   // your code here
