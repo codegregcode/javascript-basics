f8e42c93816b31de931521c3e4155e4d80ab2b82
Greg Cain <codegregcode@gmail.com>
2022-11-28 19:42:32 +0000

    fixes prettier problems

--- src/booleans.js
+++ src/booleans.js
@@ -3,69 +3,72 @@ function negate(a) {
     return true;
   }
   return false;
-};
+}
 
 function both(a, b) {
-if (a === true &&  b === true) {
-  return true;
-} return false;
-};
+  if (a === true && b === true) {
+    return true;
+  }
+  return false;
+}
 
 function either(a, b) {
   if (a === true || b === true) {
     return true;
-  } return false;
-};
+  }
+  return false;
+}
 
 function none(a, b) {
   if (a === false && b === false) {
     return true;
-  } return false;
-};
+  }
+  return false;
+}
 
 function one(a, b) {
   // your code here
-};
+}
 
 function truthiness(a) {
   // your code here
-};
+}
 
 function isEqual(a, b) {
   // your code here
-};
+}
 
 function isGreaterThan(a, b) {
   // your code here
-};
+}
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
-};
+}
 
 function isOdd(a) {
   // your code here
-};
+}
 
 function isEven(a) {
   // your code here
-};
+}
 
 function isSquare(a) {
   // your code here
-};
+}
 
 function startsWith(char, string) {
   // your code here
-};
+}
 
 function containsVowels(string) {
   // your code here
-};
+}
 
 function isLowerCase(string) {
   // your code here
-};
+}
 
 module.exports = {
   negate,
