f2e086525b93011d8867ace952b3bc5126c8d7c5
Greg Cain <codegregcode@gmail.com>
2022-11-29 15:30:08 +0000

    initialises and completes 5th (isOver65) test

--- src/objects.js
+++ src/objects.js
@@ -15,7 +15,10 @@ const hasProperty = (property, object) => {
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
