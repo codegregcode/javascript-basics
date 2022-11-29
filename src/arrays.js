ca0f50b5f5f7fe35774482562b5efb9b65ec4f3c
Greg Cain <codegregcode@gmail.com>
2022-11-29 13:45:24 +0000

    initialised and completed 13th (removeSpaces) test

--- src/arrays.js
+++ src/arrays.js
@@ -58,7 +58,7 @@ const elementsStartingWithAVowel = strings => {
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.split(' ').join('');
 };
 
 const sumNumbers = numbers => {
